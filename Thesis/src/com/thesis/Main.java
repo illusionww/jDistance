6037e704951ac0c3dfeb672429f80304751968bd
spirit93 <vi_ts@mail.ru>
2014-12-09 00:49:24 +0400

    new Main

--- Thesis/src/com/thesis/Main.java
+++ Thesis/src/com/thesis/Main.java
@@ -21,29 +21,50 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         Parser parser = new GraphMLParser("myRandomGraphn100k5pin0_3pout0_02_graphml.graphml");
-        // Parser parser = new SimpleGraphParser("swing.simplegraph");
+        //Parser parser = new SimpleGraphParser("swing.simplegraph");
+        //Parser parser = new GraphMLParser("dcrGraph_n500k5pin03pout015.graphml");
         Graph graph = parser.parse();
 
+
+        File file = new File("result1column.txt");
+        File file2 = new File("result2column.txt");
         float[][] sparseM = graph.getSparseM();
         ArrayList<SimpleNodeData> simpleNodeData = graph.getSimpleNodeData();
+        try {
+            if (file.exists() || file.createNewFile() && (file2.exists() || file2.createNewFile())) {
+                try (PrintWriter out = new PrintWriter(file.getAbsoluteFile())) {
+                    try (PrintWriter out2 = new PrintWriter(file2.getAbsoluteFile())) {
+                        for (float i = 0.001f; i < 100.1f; ++i) {
 
-        FloatMatrix A = new FloatMatrix(sparseM);
-        float[][] D = Distances.LOGARITHMIC_COMMUNICABILITY.getD(A, (float) 0.5).toArray2();
-        PrintUtils.printArray(D, "D");
 
-        Classifier classifier = new Classifier(D, simpleNodeData);
+                            FloatMatrix A = new FloatMatrix(sparseM);
+                            float[][] D = Distances.PLAIN_FOREST.getD(A, i/10).toArray2();
 
-        Integer k = 9;
-        Float p = 0.9f;
-        ArrayList<SimpleNodeData> data = classifier.predictLabel(k, p);
-        Integer countErrors = 0;
-        for (int i = 0; i < data.size(); ++i) {
-            if (!simpleNodeData.get(i).getLabel().equals(data.get(i).getLabel()) && simpleNodeData.get(i).getName().equals(data.get(i).getName()))
-                countErrors += 1;
+                            Classifier classifier = new Classifier(D, simpleNodeData);
+
+                            Integer k = 1;
+                            Float p = 0.4f;
+                            ArrayList<SimpleNodeData> data = classifier.predictLabel(k, p);
+
+                            Integer countErrors = 0;
+                            for (int q = 0; q < data.size(); ++q) {
+                                if (!simpleNodeData.get(q).getLabel().equals(data.get(q).getLabel()) && simpleNodeData.get(q).getName().equals(data.get(q).getName()))
+                                    countErrors += 1;
+                            }
+
+
+                            out.println(i/10);
+                            out2.println((float) countErrors / data.size());
+                        }
+                    }
+                }
+            }
+            else {
+                throw new FileNotFoundException();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        System.out.println(countErrors);
-        System.out.println(data.size());
     }
 
     public static void write(String fileName, int[][] sparseMatrix) {
