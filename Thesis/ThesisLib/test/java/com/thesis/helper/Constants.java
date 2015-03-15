cda12750ec66c56c54263d2ee5c8ab4ced9d1d7b
Vladimir Ivashkin <illusionww@gmail.com>
2015-03-15 13:06:07 +0300

    hm

--- Thesis/ThesisLib/test/java/com/thesis/helper/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.thesis.helper;
-
-public class Constants {
-    public static final String GNUPLOT_PATH = "C:\\cygwin64\\bin\\gnuplot.exe";
-    public static final String GRAPH_FOLDER = "C:\\Users\\illus_000\\Documents\\GitHub\\thesis\\Thesis\\graphEx\\";
-    public static final String IMG_FOLDER = "C:\\";
-
-    public static final String GRAPHML_EXAMPLE1 = "myRandomGraphn100k5pin0_3pout0_02_graphml.graphml";
-}
