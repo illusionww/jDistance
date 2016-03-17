f366ca7429094800879f04105703544817165221
Vladimir Ivashkin <illusionww@gmail.com>
2016-03-17 10:31:58 +0300

    merge with transformation

--- jDistance/src/main/java/com/jdistance/utils/Cloneable.java
+++
@@ -1,5 +0,0 @@
-package com.jdistance.utils;
-
-public interface Cloneable<T> extends java.lang.Cloneable {
-    T clone();
-}
--- jDistance/src/main/java/jeigen/ComplexDenseMatrix.java
+++
@@ -1,7 +0,0 @@
-package jeigen;
-
-public class ComplexDenseMatrix extends DenseMatrixComplex {
-    public ComplexDenseMatrix(DenseMatrixComplex A) {
-        super(A.real, A.imag);
-    }
-}
